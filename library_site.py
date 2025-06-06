7f492bacb1547c38ac6fd00471daf040c602e7e8
Rossana Suarez <46635001+roxsross@users.noreply.github.com>
2025-06-06 14:06:35 -0300

    Update library_site.py

--- library_site.py
+++ library_site.py
@@ -37,7 +37,8 @@ class Book(db.Model):
         return "ID: {} Book name: {}, Author name: {}, Publisher name: {}, Availability: {}".format(self.id, self.title, self.author, self.publisher, self.available)
 
 ## VIEW FUNCTIONS -- HAVE FORMS
-db.create_all()
+with app.app_context():
+    db.create_all()
 @app.route('/')
 def index():
     return render_template('home.html')
